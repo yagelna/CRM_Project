f8b753bee7ef6781582f2f33a450f040383b6e9a
yagelna <yagelnahshon@gmail.com>
2025-01-15 13:12:59 +0200

    env setup updated

--- backend/crm_project/deployment_settings.py
+++ backend/crm_project/deployment_settings.py
@@ -40,3 +40,11 @@ DATABASES = {
         conn_max_age=600
     )
 }
+
+# Email settings using Gmail and environment variables
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
